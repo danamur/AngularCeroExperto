025060b4a43364710ce26f290c26f199d810bfd1
Gamb <d.namur.n@gmail.com>
2020-10-12 21:16:57 -0300

    Comenzando el 5to modulo sobre "pippes de angular", se inicializo una nueva aplicacion y utilizamos unos ejemplos basicos algunos pipes.

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  nombre: string = 'Capitan América';
+  PI: number = Math.PI;
+
+}
