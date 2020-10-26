f725e45d7e9d4611751a4d384335549f183d755b
Gamb <d.namur.n@gmail.com>
2020-10-26 11:27:52 -0300

    AppSpotify terminada junto con agregado extra de alert con manejo de errores de nuestra aplicación

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -11,16 +11,23 @@ export class HomeComponent {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  errorMessage: string;
 
   constructor( private spotify: SpotifyService ) {
     
     this.loading = true;
+    this.error = false;
 
     this.spotify.getNewReleases()
       .subscribe( (data:any) =>{
         console.log(data);
         this.nuevasCanciones = data;
         this.loading = false;
+      }, ( errorServicio ) => {
+        this.loading = false;
+        this.error = true;
+        this.errorMessage = errorServicio.error.error.message;
       });
 
   }
